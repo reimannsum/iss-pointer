99ca13f71dcc997e61de23b7b18a893c9f7cf489
burke <burkereimann@gmail.com>
2021-07-01 17:03:36 -0400

    adding functionality to the Angle class and adding tests

--- bin/Angle.py
+++ bin/Angle.py
@@ -1,25 +1,29 @@
-import math
+
 
 class Angle:
 
     def __float__(self):
-        return self.angle
+        return float(self.angle)
 
     def __init__(self, measure=0.0, max_angle=360):
-        self.max = float(Angle(max_angle))   # this keeps things within 360 degrees
+        self.max = max_angle
         self.angle = self.correct_angle_size(measure)
-
-
 
     def __add__(self, other):  # Define the  + function
         other = float(other)
         total = self.angle + other
-        return self.correct_angle_size(total)
+        return Angle(self.correct_angle_size(total))
 
     def __sub__(self, other):  # Define the - function
         other = float(other)
-        total = self.angle - other
-        return self.correct_angle_size(total)
+        return self.__add__(0 - other)
+
+    def __iadd__(self, other):  # Define the += function
+        return self + float(other)
+
+    def __eq__(self, other):
+        other = float(other)
+        return self.angle == other
 
     def __repr__(self):  # Define what is displayed when printed
         if self.angle > 180:
@@ -27,11 +31,8 @@ class Angle:
         else:
             return str(self.angle)
 
-    def __iadd__(self, other):  # Define the += function
-        return Angle(self + float(other))
-
     def correct_angle_size(self, angle):
-        while math.abs(angle) > 360:
+        while abs(angle) > 360:
             if angle > 360:
                 angle -= 360
             if angle < -360:
--- tests/test_angle.py
+++ tests/test_angle.py
@@ -7,16 +7,15 @@ from pytest import approx
 from bin.Angle import Angle
 
 
-@pytest.fixture
-def angle_negative_five():
-	return Angle(-5)
+
 
 def test_initial_conditions():
 	test_0 = Angle()
 	test_180 = Angle(180)
-	assert 0.0 == 0.0
-	assert test_0.angle == approx(0)
-	assert test_180.angle == approx(180)
+	test_big = Angle(540)
+	assert approx(0) == test_0
+	assert approx(180) == test_180
+	assert approx(180) == test_big
 
 
 # noinspection PyTypeChecker
@@ -28,11 +27,15 @@ def test_addition_and_subtraction():
 	assert (test + -5) == 355	 # angles underflow when adding a negative
 
 def test_adding_Angles_together():
-	test = Angle
+	test_0 = Angle()
 	test_180 = Angle(180)
-	Test_negative_5 = Angle(-5)
-	assert (test + test_180).angle == 180
-
+	test_negative_5 = Angle(-5)
+	result = test_0 + test_180
+	assert result == 180
+	result = test_0 + test_negative_5
+	assert result == 355
+	result += test_180
+	assert result == 175
 
 
 
